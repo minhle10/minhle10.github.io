67de585bf0c972ff51daff634e8b79b2d803b872
Minh P. Le <pmle1210@gmail.com>
2023-08-11 03:54:24 +0000

    Add Projects

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -14,7 +14,8 @@ import projectImg from "./../images/example-project.jpg";
 import ethgpt from "./../images/ethgpt.png";
 import harikablog from "./../images/harikablog.png";
 import gardwebapp from "./../images/gardwebapp.png";
-import { fadeIn } from "../utils/motion";
+import cbitesLanding from "./../images/cbitesLanding.png";
+import { fadeIn, fadeUp } from "../utils/motion";
 
 /**
  * Desk image
@@ -25,10 +26,8 @@ import { fadeIn } from "../utils/motion";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
 import { SectionWrapper } from "../hoc";
 
-const imageAltText = "purple and blue abstract background";
 /**
  * Project list
  *
@@ -37,20 +36,12 @@ const imageAltText = "purple and blue abstract background";
  */
 const projectList = [
   {
-    title: "Eth-GPT",
+    title: "Crypto Bites Landing Page",
     description:
-      "Created a web application that injects data into Chat-GPT using vector embeddings and cosine similiarity to generate reliable responses to Ethereum specific questions.",
-    src: ethgpt,
-    url: "https://eth-gpt-rho.vercel.app/",
-    tags: [],
-  },
-  {
-    title: "Harika Blog",
-    description:
-      "Built a dynamic blog application in which users can upload complex read, write, and update complex files and rich-text.",
-    src: harikablog,
-    url: "https://nameless-lake-28361.herokuapp.com/",
-    tags: [],
+      "Designed and implemented the landing page for Crypto Bites App, our iOS app. Also, currently building out our iOS App.",
+    src: cbitesLanding,
+    url: "https://cbites.co/",
+    tags: ["React", "framer-motion", "Tailwind", "3js", "React-Native"],
   },
   {
     title: "Gard Web App",
@@ -58,48 +49,59 @@ const projectList = [
       "Helped to integrate a robust database system on the backend and contributed to translating various UX/UI designs to clean front-end code using best practices.",
     src: gardwebapp,
     url: "https://gard.money/",
-    tags: [],
+    tags: ["React", "Javascript", "Firebase", "CSS3", "HTML5", "Figma"],
   },
   {
-    title: "Crypto Bites Landing Page",
-    description: "Designed and implemented the landing page for Crypto Bites App, our iOS app.",
-    src: projectImg,
-    url: "https://cbites.co/",
-    tags: [],
+    title: "Eth-GPT",
+    description:
+      "Created a web application that injects data into Chat-GPT using vector embeddings and cosine similiarity to generate reliable responses to Ethereum specific questions.",
+    src: ethgpt,
+    url: "https://eth-gpt-rho.vercel.app/",
+    tags: ["Vercel", "Tailwind", "OpenAI", "Ethereum", "supabase"],
+  },
+  {
+    title: "Harika Blog",
+    description:
+      "Built a dynamic blog application in which users can upload complex read, write, and update complex files and rich-text.",
+    src: harikablog,
+    url: "https://nameless-lake-28361.herokuapp.com/",
+    tags: ["Rails", "Postgres", "Heroku", "Boostrap", "AWS"],
   },
 ];
 
 const Portfolio = () => {
   return (
-    <section className="padding" id="portfolio">
-      <img className="background" src={image} alt={imageAltText} />
-      <motion.h2 variants={fadeIn("", "", 0.1, 1)} style={{ textAlign: "center" }}>
-        Portfolio
-      </motion.h2>
+    <section style={{ backgroundColor: "#090a0b" }} className="padding" id="portfolio">
+      <motion.div initial="offscreen" whileInView="onscreen" viewport={{ once: true, amount: 0.8 }}>
+        <motion.h2 variants={fadeUp} style={{ textAlign: "center", color: "#fff" }}>
+          Portfolio
+        </motion.h2>
+      </motion.div>
+
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div className="container">
-          {projectList.map((project, index) => {
-            return (
-              <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} key={index}>
-                <div
+          {projectList.map((project, index) => (
+            <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} key={index}>
+              <div
+                style={{
+                  background: "#151619",
+                  position: "relative",
+                  borderTopRightRadius: "0.8rem",
+                  borderTopLeftRadius: "0.8rem",
+                  overflow: "hidden",
+                  height: "auto",
+                }}
+              >
+                <motion.div
                   style={{
-                    background: "#d6ebff",
                     position: "relative",
-                    borderTopRightRadius: "0.8rem",
-                    borderTopLeftRadius: "0.8rem",
-                    overflow: "hidden",
-                    height: "auto",
+                    bottom: -10,
+                    padding: "1em 1em 0px",
+                    display: "flex",
+                    justifyContent: "center",
                   }}
                 >
-                  <motion.div
-                    style={{
-                      position: "relative",
-                      bottom: -10,
-                      padding: "1em 1em 0px",
-                      display: "flex",
-                      justifyContent: "center",
-                    }}
-                  >
+                  <a href={project.url} style={{ display: "flex", justifyContent: "center" }}>
                     <motion.img
                       src={project.src}
                       alt="example project"
@@ -114,28 +116,51 @@ const Portfolio = () => {
                         borderTopRightRadius: "0.4rem",
                         borderTopLeftRadius: "0.4rem",
                         margin: "auto",
-                      }}
-                    />
-                  </motion.div>
-                  {/* <a href={project.url} target="_blank" rel="noopener noreferrer">
-                    Link
-                  </a> */}
-                </div>
-                <div
-                  style={{
-                    backgroundColor: "#dae7ff",
-                    borderBottomRightRadius: "0.8rem",
-                    borderBottomLeftRadius: "0.8rem",
-                    padding: "2em",
-                    height: "auto",
-                    boxShadow: "inset 0px 3px 3px rgba(50, 50, 50, 0.75)",
-                  }}
-                >
-                  {project.description}
-                </div>
-              </motion.div>
-            );
-          })}
+                      }} />
+                  </a>
+                </motion.div>
+                {/* <a href={project.url} target="_blank" rel="noopener noreferrer">
+                              Link
+                            </a> */}
+              </div>
+              <div
+                style={{
+                  color: "#fff",
+                  backgroundColor: "#151619",
+                  padding: "2em",
+                  height: "auto",
+                  textAlign: "center",
+                }}
+              >
+                {project.description}
+              </div>
+              <a
+                href={project.url}
+                style={{
+                  color: "#6b91ea",
+                  backgroundColor: "#151619",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                {project.url}
+              </a>
+              <div
+                style={{
+                  color: "#A8C3A7",
+                  backgroundColor: "#151619",
+                  borderBottomRightRadius: "0.8rem",
+                  borderBottomLeftRadius: "0.8rem",
+                  padding: "2em",
+                  textAlign: "center",
+                }}
+              >
+                {project.tags.map((tag) => {
+                  return `#${tag} `;
+                })}
+              </div>
+            </motion.div>
+          ))}
         </div>
       </div>
     </section>
